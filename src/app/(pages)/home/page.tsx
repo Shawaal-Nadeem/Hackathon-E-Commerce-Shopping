4724b7a09783610898b3243e866cad83a61e2c51
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-06-10 00:25:57 +0500

    working on navbar (position)

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
  const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
+
+    </div>
   )
 }
 export default Home;
