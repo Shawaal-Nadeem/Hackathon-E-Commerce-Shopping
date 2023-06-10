d1c7b716ca8fc99615a63ffbfa9548353a7dd8dc
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-06-10 15:55:39 +0500

    Homepage on working

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
-
+import Style from './home.module.css'
+import { LuShoppingCart } from 'react-icons/lu'
+import Link from 'next/link'
  const Home = () => {
   return (
-    <div>Home
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-      <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, nulla iste nesciunt voluptatibus corrupti laborum iure, dolor vel autem hic? Magni excepturi explicabo nam ipsa aut neque recusandae sunt.</div>
-
+    <div className={Style.home}>
+      <div className={Style.content}>
+      <div className={Style.col1}>
+      <button className={Style.saleBtn}>Sale 70%</button>
+      <h1>An Industrial Take on Streetwear</h1>
+      <p>Anyone can beat you but no one can beat your outfit as long as you wear Dine outfits.</p>
+      <Link href={'/all_products'}><button className={Style.shopingBtn}>
+<LuShoppingCart size={20}/>
+        Start Shopping
+        </button></Link>
+        <div className={Style.logoPics}>
+          <img src='https://hackathon-dine-market.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero1.66abddd4.png&w=96&q=75' alt='Loading...'></img>
+          <img src='https://hackathon-dine-market.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero2.247cd605.png&w=96&q=75' alt='Loading...'></img>
+          <img src='https://hackathon-dine-market.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero3.6076521d.png&w=96&q=75' alt='Loading...'></img>
+          <img src='https://hackathon-dine-market.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero4.0489f1fc.png&w=96&q=75' alt='Loading...'></img>
+        </div>
+      </div>
+    <div className={Style.col2}><img src='/assets/images/homepic.png' alt='Loading...'></img></div>
+      </div>
     </div>
   )
 }
