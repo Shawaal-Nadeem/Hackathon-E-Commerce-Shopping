5720ce808f71c7fcb6ebf0302ba5a0a0dae4bff4
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-06-15 15:07:52 +0500

    Shopping Cart Page and Responsiveness Completed

--- src/app/(pages)/shopping-cart/page.tsx
+++ src/app/(pages)/shopping-cart/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Style from './cart.module.css'
+import {FiShoppingBag} from 'react-icons/fi'
  const ShoppingCart = () => {
   return (
-    <div>ShoppingCart</div>
+    <div className={Style.main}>
+    <div className={Style.subMain}>
+    <h2 >Shopping Cart</h2>
+    <div>
+      <FiShoppingBag size={150}/>
+      <p>Your shopping bag is empty</p>
+    </div>
+    </div>  
+      </div>
   )
 }
 export default ShoppingCart;
