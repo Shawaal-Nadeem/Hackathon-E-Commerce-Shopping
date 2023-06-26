48c33d0ea43e4ca8bad5419ca2e4eb62185d9e1f
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-06-27 00:14:39 +0500

    Dynamic Data on working

--- src/app/(pages)/female/page.tsx
+++ src/app/(pages)/female/page.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import products from '@/app/utils/mock';
 import Image from 'next/image';
+
  const Female = () => {
   const femaleProducts = products.filter(function(check){return check.category=='female'});
 function femaleData(data:any)
 {
 return(
   <div key={data.id}>
+    <Image src={data.image} width={270} height={270} alt={data.name}/>
     <h1>{data.name}</h1>
+    <h3>{data.cloth_Type}</h3>
     <p>{data.price}</p>
-    <Image src={data.image} width={200} height={200} alt={data.name}/>
   </div>
 )
 }
   return (
-    <div>Female
-      <div>
+    <div>
+      <div className='productCards'>
         {femaleProducts.map(femaleData)}
       </div>
     </div>
--- src/app/utils/mock.ts
+++ src/app/utils/mock.ts
@@ -4,17 +4,19 @@ const products = [
     name: "product1",
     slug:"product-1",
     category:"female",
-    price:250,
-    image:"https://hackathon-dine-market.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpromotion2.b5f201ac.png&w=384&q=75"
+    price:"$175",
+    cloth_Type:"$Sweater",
+    image:"https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fbf4d73c91b7c7c2875fbfbb8e1a9c8113c82e90a-370x394.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75"
   },
 
   {
-    id:2,
-    name:"product2",
+    id: 2,
+    name: "product2",
     slug:"product-2",
-    category:"male",
-    price:100,
-    image:"https://hackathon-dine-market.vercel.app/products/raglan-sweatshirt"
+    category:"female",
+    price:"$525",
+    cloth_Type:"$Jackets",
+    image:"https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fbfd5b4b468e1a6c84c25589fae1a103cac342102-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75"
   },
 ];
 export default products;
