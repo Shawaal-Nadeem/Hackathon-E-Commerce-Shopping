12fd276aea22c975f03502d2e90f79e41a87ad9a
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-06-27 13:07:45 +0500

    Female Page Completed

--- src/app/(pages)/female/page.tsx
+++ src/app/(pages)/female/page.tsx
@@ -3,22 +3,24 @@ import products from '@/app/utils/mock';
 import Image from 'next/image';
 
  const Female = () => {
-  const femaleProducts = products.filter(function(check){return check.category=='female'});
+  const femaleProducts = products.filter(function(check){return check.category==='female'});
 function femaleData(data:any)
 {
 return(
-  <div key={data.id}>
+  <div key={data.id} className='eachCard'>
     <Image src={data.image} width={270} height={270} alt={data.name}/>
-    <h1>{data.name}</h1>
-    <h3>{data.cloth_Type}</h3>
-    <p>{data.price}</p>
+    <h1 className='cardName'>{data.name}</h1>
+    <h3 className='clothType'>{data.cloth_Type}</h3>
+    <p className='priceCard'>{data.price}</p>
   </div>
 )
 }
   return (
-    <div>
+    <div className='productsMain'>
+      <div className='content'>
       <div className='productCards'>
         {femaleProducts.map(femaleData)}
+      </div>
       </div>
     </div>
   )
--- src/app/utils/mock.ts
+++ src/app/utils/mock.ts
@@ -1,22 +1,122 @@
 const products = [
-  {
-    id: 1,
-    name: "product1",
-    slug:"product-1",
-    category:"female",
-    price:"$175",
-    cloth_Type:"$Sweater",
-    image:"https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fbf4d73c91b7c7c2875fbfbb8e1a9c8113c82e90a-370x394.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75"
+    {
+        name: "Raglan Sweatshirt",
+        slug: "Raglan-Sweatshirt",
+        category: "male",
+        price: "$195",
+        cloth_Type: "$Sweater",
+        image:
+          "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2F2228e40aa700a68031eff4016f9e625ade813a89-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+    },
+
+    {
+    name: "Flex Sweatshirt",
+    slug: "Flex-Sweatshirt",
+    category: "female",
+    price: "$175",
+    cloth_Type: "$Sweater",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fbf4d73c91b7c7c2875fbfbb8e1a9c8113c82e90a-370x394.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
   },
 
   {
-    id: 2,
-    name: "product2",
-    slug:"product-2",
-    category:"female",
-    price:"$525",
-    cloth_Type:"$Jackets",
-    image:"https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fbfd5b4b468e1a6c84c25589fae1a103cac342102-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75"
+    name: "Imperial Alpaca Hoodie",
+    slug: "Imperial-Alpaca-Hoodie",
+    category: "female",
+    price: "$525",
+    cloth_Type: "$Jackets",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fbfd5b4b468e1a6c84c25589fae1a103cac342102-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+  },
+
+  {
+    name: "Flex Push Button Bomber",
+    slug: "Flex-Push-Button-Bomber",
+    category: "male",
+    price: "$224",
+    cloth_Type: "$Sweater",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2F4e2ed6a9eaa6e1413843e53f3113ccfd2104c301-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+  },
+
+  {
+    name: "Muscle Tank",
+    slug: "Muscle-Tank",
+    category: "female",
+    price: "$75",
+    cloth_Type: "$T Shirts",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fd8cbcd5d002fade4d83dc66f910f34ae84d61975-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+  },
+
+  {
+    name: "Loose Control",
+    slug: "Loose-Control",
+    category: "kids",
+    price: "$110",
+    cloth_Type: "$T Shirts",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2F2caaff3cc1daa4b4b5e45afd4ce6e27bf026fdd7-600x600.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+  },
+
+  {
+    name: "Cameryn Sash Tie Dress",
+    slug: "Cameryn-Sash-Tie-Dress",
+    category: "female",
+    price: "$545",
+    cloth_Type: "$Dress",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fffc858fc182553bee2aaff34fe728bf07d15f2b5-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+  },
+
+  {
+    name: "Brushed Raglan Sweatshirt",
+    slug: "Brushed-Raglan-Sweatshirt",
+    category: "female",
+    price: "$195",
+    cloth_Type: "$Sweater",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fa6a38f6a1f31dafe5f3294a4384f865b7d25a344-370x394.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+  },
+
+  {
+    name: "Brushed Bomber",
+    slug: "Brushed-Bomber",
+    category: "female",
+    price: "$224",
+    cloth_Type: "$Jackets",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2F994bf21135a265d4bbeff47cd8fddae6082a0b26-370x394.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+  },
+
+  {
+    name: "Lite Sweatpants",
+    slug: "Lite-Sweatpants",
+    category: "female",
+    price: "$150",
+    cloth_Type: "$Pants",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2F7739d01f547a127eca9fdfdb267935974e8d2b83-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+  },
+
+  {
+    name: "Pink Fleece Sweatpants",
+    slug: "Pink-Fleece-Sweatpants",
+    category: "female",
+    price: "$195",
+    cloth_Type: "$Pants",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fc7b4f3b93e9536ce55353fdd05b7a81a2b57be02-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
+  },
+
+  {
+    name: "Flex Sweatpants",
+    slug: "Flex-Sweatpants",
+    category: "female",
+    price: "$175",
+    cloth_Type: "$Pants",
+    image:
+      "https://hackathon-dine-market.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fzwnaaffj%2Fproduction%2Fed4960c6cf46ac416ae2fa6c0a3accbaa551457e-278x296.png%3Fw%3D400%26fit%3Dmax%26auto%3Dformat&w=640&q=75",
   },
 ];
 export default products;
