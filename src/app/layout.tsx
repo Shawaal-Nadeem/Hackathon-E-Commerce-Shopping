60608083b5767c107bac7aeaa61efe3b17e5aaf9
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-07-22 13:07:31 +0500

    Font Problem solved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Sora:wght@100;200;300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+        </head>
       <body className={inter.className}>
         <Navbar/>
         {children}
